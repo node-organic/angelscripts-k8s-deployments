43a9b0df23794faa0f7b0eef6dfb339e9df34830
outbounder <obiwonn@gmail.com>
2019-03-21 16:54:29 +0200

    naive implementation of cell logs

--- tasks/logs.js
+++ tasks/logs.js
@@ -1,9 +1,61 @@
 const path = require('path')
+const {exec} = require('child_process')
+const chalk = require('chalk')
+chalk.enabled = true
+chalk.level = 3
+
+const findSkeletonRoot = require('organic-stem-skeleton-find-root')
+
+const colorMap = ['white', 'orange', 'green', 'silver', 'blue', 'yellow']
 
 module.exports = function (angel) {
-  angel.on('logs', function () {
+  angel.on('logs', async function () {
     let packagejson = require(path.join(process.cwd(), 'package.json'))
-    let kubetailScript = path.resolve(__dirname, '../bin/kubetail.sh')
-    angel.exec(`/bin/bash ${kubetailScript} --selector app=${packagejson.name} --since 10m --timestamps`)
+
+    let pods = await getPodsForCell(packagejson.name)
+    pods.forEach(function (pod, podIndex) {
+      let cmd = `kubectl logs -f ${pod} --timestamps --since 10m`
+      let podColor = colorMap[podIndex]
+      console.log(chalk.keyword(podColor)(cmd))
+      let child = exec(cmd)
+      child.stdout.on('data', function (chunk) {
+        console.log(chalk.bgKeyword('blue')(podIndex.toString()), chalk.keyword(podColor)(chunk.toString()))
+      })
+      child.stderr.on('data', function (chunk) {
+        console.log(chalk.bgKeyword('blue')(podIndex.toString()), chalk.keyword(podColor)(chunk.toString()))
+      })
+    })
+  })
+}
+
+const getDeploymentSelectorsForCell = function (cellInfo) {
+  let deployments = cellInfo.dna.deployment
+  if (!Array.isArray(deployments)) deployments = [deployments]
+
+  for (let i = 0; i < deployments.length; i++) {
+    if (deployments[i].kind === 'Deployment') {
+      return deployments[i].spec.selector.matchLabels
+    }
+  }
+}
+
+const getPodsForCell = async function (cellName) {
+  const root = await findSkeletonRoot()
+  const loadCellInfo = require(path.join(root, 'cells/node_modules/lib/load-cell-info'))
+  const cellInfo = await loadCellInfo(cellName)
+  let matchLabelsSelector = getDeploymentSelectorsForCell(cellInfo)
+  if (!matchLabelsSelector) throw new Error('failed to find deployment for ' + cellName)
+  const labels = []
+  for (let key in matchLabelsSelector) {
+    labels.push(`-l=${key}=${matchLabelsSelector[key]}`)
+  }
+  return new Promise((resolve, reject) => {
+    let cmd = `kubectl get pods ${labels.join(' ')} --no-headers -o name`
+    exec(cmd, function (err, stdout, stderr) {
+      if (err) return reject(err)
+      resolve(stdout.split('\n').map((value) => {
+        return value.replace('pod/', '')
+      }).filter(v => v))
+    })
   })
 }
