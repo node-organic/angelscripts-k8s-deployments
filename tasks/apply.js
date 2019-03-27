84972797a0d581ca03e797406021dc2be832b226
outbounder <obiwonn@gmail.com>
2019-03-27 18:39:30 +0200

    add support for force apply task

--- tasks/apply.js
+++ tasks/apply.js
@@ -2,4 +2,7 @@ module.exports = function (angel) {
   angel.on('apply :path', async function (angel) {
     require('../lib/organic-kubectl')(angel, 'apply', angel.cmdData.path)
   })
+  angel.on('fapply :path', async function (angel) {
+    require('../lib/organic-kubectl')(angel, 'apply --force', angel.cmdData.path)
+  })
 }
