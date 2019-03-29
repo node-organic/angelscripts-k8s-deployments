5ad3d428a60fe5fb121b92219717519cd9865491
outbounder <obiwonn@gmail.com>
2019-03-29 17:34:51 +0200

    fix index require map

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = async function (angel) {
   require('angelabilities-exec')(angel)
   require('./tasks/apply')(angel)
   require('./tasks/build')(angel)
-  require('./tasks/compose')(angel)
   require('./tasks/delete')(angel)
   require('./tasks/docker')(angel)
   require('./tasks/logs')(angel)
