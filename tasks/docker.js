ccb8260d8b4c2596cfa290eaa00cc45e2577da7a
outbounder <obiwonn@gmail.com>
2019-03-30 17:17:27 +0200

    npm install only production in docker

--- tasks/docker.js
+++ tasks/docker.js
@@ -17,11 +17,11 @@ COPY ./dist /usr/share/nginx/html
 RUN apk update && apk upgrade && \
   apk add --no-cache bash git openssh
 COPY ${cellInfo.dna.cwd}/package*.json ${cellInfo.dna.cwd}/
-RUN cd ${cellInfo.dna.cwd} && npm install
+RUN cd ${cellInfo.dna.cwd} && npm install --production
 COPY cells/node_modules/server/package*.json cells/node_modules/server/
-RUN cd cells/node_modules/server && npm install
+RUN cd cells/node_modules/server && npm install --production
 COPY cells/node_modules/lib/package*.json cells/node_modules/lib/
-RUN cd cells/node_modules/lib && npm install
+RUN cd cells/node_modules/lib && npm install --production
 COPY . .
 WORKDIR ${cellInfo.dna.cwd}
 ENV CELL_MODE _production
